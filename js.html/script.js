f642f594c5fbfdc39811db9ceb0ba4578957bc6d
Biel-gabriel <gabrielelianas2@gmail.com>
2025-04-03 21:30:01 -0300

    Add files via upload

---
+++ js.html/script.js
@@ -0,0 +1,9 @@
+var titulo = document.getElementById("titulo)"
+var btn = document.getElementById("#btn")
+"titulo.innerText = "Gabriel Luiz")
+
+function dispararAlerta(){}
+alert("Gabriel Luiz")
+}
+
+btn.addEventListener("click", dispararAlerta)
